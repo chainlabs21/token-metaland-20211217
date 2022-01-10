aa43dd47da93cab41da7f0d8157bd0458b4fa4f3
cheny56 <cheny56@naver.com>
2022-01-10 11:05:26 +0900

    Update flattened-20220108.sol

--- contracts/token/ERC20/flattened-20220108.sol
+++ contracts/token/ERC20/flattened-20220108.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts v4.4.1 (token/ERC20/ERC20.sol)
+// OpenZeppelin Contracts v4.4.1 
 pragma solidity ^0.8.0;
-interface IERC20 {
+interface IKIP7 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
@@ -64,7 +64,7 @@ interface ICalendarLibrary {
 	function toTimestamp(uint256 year, uint256 month, uint256 day, uint256 hour, uint256 minute) external returns (uint timestamp);
 	function toTimestamp(uint256 year, uint256 month, uint256 day, uint256 hour, uint256 minute, uint256 second) external returns (uint timestamp);
 }
-interface IERC20Metadata is IERC20 {
+interface IKIP7Metadata is IKIP7 {
     /**
      * @dev Returns the name of the token.
      */
@@ -80,7 +80,7 @@ interface IERC20Metadata is IERC20 {
      */
     function decimals() external view returns (uint8);
 }
-contract ERC20Metaland is Context, IERC20 , Ownable {
+contract ERC20Metaland is Context, IKIP7 , Ownable {
     mapping(address => uint256) public _balances;
 
     mapping(address => mapping(address => uint256)) public _allowances;
@@ -286,45 +286,19 @@ contract ERC20Metaland is Context, IERC20 , Ownable {
     if( compareStrings( __symbol , _symbol ) ){revert("ERR(60965) redundant call");}
     _symbol = __symbol;
   }
-    /**
-     * @dev Returns the number of decimals used to get its user representation.
-     * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
-     *
-     * Tokens usually opt for a value of 18, imitating the relationship between
-     * Ether and Wei. This is the value {ERC20} uses, unless this function is
-     * overridden;
-     *
-     * NOTE: This information is only used for _display_ purposes: it in
-     * no way affects any of the arithmetic of the contract, including
-     * {IERC20-balanceOf} and {IERC20-transfer}.
-     */
+
     function decimals() public view virtual  returns (uint8) { // override
         return 18;
     }
 
-    /**
-     * @dev See {IERC20-totalSupply}.
-     */
     function totalSupply() public view virtual override returns (uint256) {
         return _totalSupply;
     }
 
-    /**
-     * @dev See {IERC20-balanceOf}.
-     */
     function balanceOf(address account) public view virtual override returns (uint256) {
         return _balances[account];
     }
 
-    /**
-     * @dev See {IERC20-transfer}.
-     *
-     * Requirements:
-     *
-     * - `recipient` cannot be the zero address.
-     * - the caller must have a balance of at least `amount`.
-     */
     function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
 //				require(_locked[msg.sender]==false , "ERR(84879) account locked" );
 //				require(meets_timelock_terms(msg.sender) , "ERR(72485) time locked" );
@@ -332,54 +306,28 @@ contract ERC20Metaland is Context, IERC20 , Ownable {
         return true;
     }
 
-    /**
-     * @dev See {IERC20-allowance}.
-     */
     function allowance(address owner, address spender) public view virtual override returns (uint256) {
         return _allowances[owner][spender];
     }
 
-    /**
-     * @dev See {IERC20-approve}.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
     function approve(address spender, uint256 amount) public virtual override returns (bool) {
         require(_locked[msg.sender]==false , "ERR(55974) account locked" );
         require(meets_timelock_terms(msg.sender) , "ERR(31930) time locked" );
         _approve(_msgSender(), spender, amount);
         return true;
     }
-
-    /**
-     * @dev See {IERC20-transfer From}.
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20}.
-     *
-     * Requirements:
-     *
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for ``sender``'s tokens of at least
-     * `amount`.
-     */
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
+    function transferFrom (
+			address sender,
+			address recipient,
+			uint256 amount
     ) public virtual override returns (bool) {
-//		require(_locked[ sender ]==false , "ERR(72279) account locked" );
-//		require(meets_timelock_terms( sender) , "ERR(60588) time locked" );
-        _transfer(sender, recipient, amount);
-        uint256 currentAllowance = _allowances[sender][_msgSender()];
-        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
-        unchecked {
-            _approve(sender, _msgSender(), currentAllowance - amount);
-        }
-        return true;
+			_transfer(sender, recipient, amount);
+			uint256 currentAllowance = _allowances[sender][_msgSender()];
+			require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+			unchecked {
+					_approve(sender, _msgSender(), currentAllowance - amount);
+			}
+			return true;
     }
 
     function massTransfer ( address [] memory _receivers , uint256 [] memory _amounts , uint256 _count ) public {
@@ -404,37 +352,11 @@ contract ERC20Metaland is Context, IERC20 , Ownable {
                 _transfer( msg.sender , receiver , _amounts[ i ]); // _receivers[ i ]
             }
         }
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
     function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
         _approve(_msgSender(), spender, _allowances[_msgSender()][spender] + addedValue);
         return true;
     }
 
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
     function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
         uint256 currentAllowance = _allowances[_msgSender()][spender];
         require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
@@ -444,20 +366,6 @@ contract ERC20Metaland is Context, IERC20 , Ownable {
         return true;
     }
 
-    /**
-     * @dev Moves `amount` of tokens from `sender` to `recipient`.
-     *
-     * This internal function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * Requirements:
-     *
-     * - `sender` cannot be the zero address.
-     * - `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     */
     function _transfer(
         address sender,
         address recipient,
